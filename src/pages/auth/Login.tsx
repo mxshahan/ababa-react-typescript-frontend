ea276fce37f8b025e027b45a7f643a1590216bdb
shahan953 <misbauddin1994@gmail.com>
2022-08-13 21:03:03 +0600

    Updated Login Task

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+/**
+ * Login Component
+ * Validate Field -> email: isEmail, isEmpty
+ * Validate Field -> password: isEmpty, minLength(6)
+ */
+
 export const Login: React.FC = () => {
   return <div>login</div>;
 };
+
